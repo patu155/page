bbc6de2b017efaed5f18ceae5e8021dcfe6848cf
patu155 <63252583+patu155@users.noreply.github.com>
2024-04-25 18:10:55 +0200

    Add files via upload

---
+++ Mikrofallówa/script.js
@@ -0,0 +1,71 @@
+const otworz1 = () => {
+  document.getElementById("produkt1").src = "/zdjęcia/amica2.png";
+}
+
+const otworz2 = () => {
+  document.getElementById("produkt2").src = "/zdjęcia/beko2.png";
+}
+
+const otworz3 = () => {
+  document.getElementById("produkt3").src = "/zdjęcia/bosh2.png";
+}
+
+const otworz4 = () => {
+  document.getElementById("produkt4").src = "/zdjęcia/lg2.png";
+}
+
+const otworz5 = () => {
+  document.getElementById("produkt5").src = "/zdjęcia/samsung_bespoke2.png";
+}
+
+const otworz6 = () => {
+  document.getElementById("produkt6").src = "/zdjęcia/samsung2.png";
+}
+
+const cena1 = () => {
+    document.getElementById("produkt1").src = "/zdjęcia/amica3.png";
+}
+
+const cena2 = () => {
+    document.getElementById("produkt2").src = "/zdjęcia/beko3.png";
+}
+
+const cena3 = () => {
+    document.getElementById("produkt3").src = "/zdjęcia/bosh3.png";
+}
+
+const cena4 = () => {
+    document.getElementById("produkt4").src = "/zdjęcia/lg3.png";
+}
+
+const cena5 = () => {
+    document.getElementById("produkt5").src = "/zdjęcia/samsung_bespoke3.png";
+}
+
+const cena6 = () => {
+    document.getElementById("produkt6").src = "/zdjęcia/samsung_grunwald3.png";
+}
+
+const leave1 = () => {
+    document.getElementById("produkt1").src = "/zdjęcia/amica.png";
+}
+
+const leave2 = () => {
+    document.getElementById("produkt2").src = "/zdjęcia/beko.png";
+}
+
+const leave3 = () => {
+    document.getElementById("produkt3").src = "/zdjęcia/bosh.png";
+}
+
+const leave4 = () => {
+    document.getElementById("produkt4").src = "/zdjęcia/lg.png";
+}
+
+const leave5 = () => {
+    document.getElementById("produkt5").src = "/zdjęcia/samsung_bespoke.png";
+}
+
+const leave6 = () => {
+    document.getElementById("produkt6").src = "/zdjęcia/samsung.png";
+}
